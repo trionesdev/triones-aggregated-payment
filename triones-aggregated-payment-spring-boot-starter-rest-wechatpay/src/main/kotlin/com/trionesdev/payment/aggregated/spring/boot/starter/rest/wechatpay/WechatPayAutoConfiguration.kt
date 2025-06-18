5a1043aa862878b6bf58d45c18fe22a1d9d20262
bane.shi <fengxiaotx@163.com>
2025-06-18 18:07:39 +0800

    feat:

--- triones-aggregated-payment-spring-boot-starter-rest-wechatpay/src/main/kotlin/com/trionesdev/payment/aggregated/spring/boot/starter/rest/wechatpay/WechatPayAutoConfiguration.kt
+++ triones-aggregated-payment-spring-boot-starter-rest-wechatpay/src/main/kotlin/com/trionesdev/payment/aggregated/spring/boot/starter/rest/wechatpay/WechatPayAutoConfiguration.kt
@@ -7,9 +7,10 @@ import org.springframework.beans.factory.ObjectProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 
 @Configuration
-@ComponentScan(basePackages = ["com.trionesdev.payment.aggregated.spring.boot.starter.rest.wechatpay"])
+@Import(value = [WechatPayResource::class])
 class WechatPayAutoConfiguration {
 
     @Bean
